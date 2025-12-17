ba8f37a387eefe3a7231d0d8a7df996be3d43f51
Shin Matsumoto <prettynighttime@gmail.com>
2025-12-17 21:03:13 +0900

    36_SpringBootでの例外処理の課題提出

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,5 +1,8 @@
 package raisetech.student.management.data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,13 +11,28 @@ import lombok.Setter;
 public class Student {
 
   private String studentUuid;
+
+  @NotBlank
   private String name;
+
+  @NotBlank
   private String furiganaName;
+
   private String nickname;
+
+  @NotBlank
+  @Email
   private String email;
+
+  @NotBlank
   private String address;
+
+  @Pattern(regexp = "\\d+$")
   private int age;
+
   private String gender;
+
   private String remark;
+
   private boolean isDeleted;
 }
